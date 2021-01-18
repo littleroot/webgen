fd894aadfbe2fee074319aefa4759dcf14b0aa02
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2021-01-19 00:54:22 +0530

    refactor tokenization into functions

--- nausicaa.go
+++ nausicaa.go
@@ -157,7 +157,7 @@ func (g *generator) generateOneFile(path string, history *orderedSet) error {
 	return nil
 }
 
-var disallowedRefs = map[string]struct{}{
+var disallowedRefNames = map[string]struct{}{
 	"roots": {},
 }
 
@@ -169,7 +169,7 @@ type TagAndVarAndTypeName struct {
 
 func (g *generator) generateComponent(in io.Reader, path string, history *orderedSet) (err error) {
 	if history.has(path) {
-		return fmt.Errorf("cyclical include") // TODO
+		return fmt.Errorf("cycle in include paths") // TODO
 	}
 
 	history.add(path)
@@ -227,100 +227,28 @@ tokenizeView:
 
 			names.push(TagAndVarName{tagName, varName})
 
-			if tagName == "include" {
-				var foundPathAttr bool
-				var refAttrVal string
-				var includeTypeName string
-
-				err := attrsFunc(z, hasAttr, func(k, v []byte) error {
-					isRef := equalsRef(k)
-					isPath := equalsPath(k)
-
-					// validate attributes
-					if !isRef && !isPath {
-						return fmt.Errorf("<include> specifies invalid attribute %q", k)
-					}
-
-					if isRef {
-						if _, ok := disallowedRefs[string(v)]; ok {
-							return fmt.Errorf("ref name %q disallowed (reserved for internal use)", v)
-						}
-						refAttrVal = string(v)
-					} else {
-						foundPathAttr = true
-
-						v := string(v)
-						var includePath string
-						if filepath.IsAbs(v) {
-							includePath = filepath.Join(g.opts.Root, v)
-						} else {
-							includePath = filepath.Join(filepath.Dir(path), v)
-						}
-						err := g.generateOneFile(includePath, history)
-						if err != nil {
-							return err
-						}
-						// ... successfully included; append it
-						includeTypeName = componentTypeName(filepath.Base(includePath))
-						includeConstructorFuncName := constructorFuncName(includeTypeName)
-						fmt.Fprintf(&funcBuf, "%s := %s()\n", varName, includeConstructorFuncName)
-					}
-					return nil
-				})
-				if err != nil {
-					return err
-				}
-
-				if !foundPathAttr {
-					return fmt.Errorf("missing required \"path\" attribute in <include>")
-				}
-				if refAttrVal != "" {
-					ex, ok := refs[refAttrVal]
-					if ok {
-						return fmt.Errorf("ref name %q appears multiple times in component (previous occurence in <%s>)", refAttrVal, ex.TagName)
-					}
-					refs[refAttrVal] = TagAndVarAndTypeName{tagName, varName, includeTypeName}
-				}
-
-				continue
+			err := g.handleStartToken(&funcBuf, z, path, tagName, varName, hasAttr, refs, history)
+			if err != nil {
+				return err
 			}
-
-			fmt.Fprintf(&funcBuf, "%s := _document.CreateElement(%q, nil)\n", varName, tagName)
-			attrsFunc(z, hasAttr, func(k, v []byte) error {
-				if equalsRef(k) {
-					v := string(v)
-					if _, ok := disallowedRefs[v]; ok {
-						return fmt.Errorf("ref name %q disallowed (reserved for internal use)", v)
-					}
-					ex, ok := refs[v]
-					if ok {
-						return fmt.Errorf("ref name %q appears multiple times in component (previous occurence in <%s>)", v, ex.TagName)
-					}
-					refs[v] = TagAndVarAndTypeName{tagName, varName, ""}
-					return nil
-				}
-				fmt.Fprintf(&funcBuf, "%s.SetAttribute(%q, %q)\n", varName, k, v)
-				return nil
-			})
 
 		case html.EndTagToken:
 			curr := names.pop()
-			parent, ok := names.peek()
-			if !ok {
-				// no parent; record as root
-				roots = append(roots, curr.VarName)
-				continue
-			}
-			if curr.TagName == "include" {
-				fmt.Fprintf(&funcBuf, "for _, r := range %s.roots {\n", curr.VarName)
-				fmt.Fprintf(&funcBuf, "%s.AppendChild(&r.Node)\n", parent.VarName)
-				fmt.Fprintf(&funcBuf, "}\n")
-			} else {
-				fmt.Fprintf(&funcBuf, "%s.AppendChild(&%s.Node)\n", parent.VarName, curr.VarName)
-			}
+			g.handleEndToken(&funcBuf, curr.TagName, curr.VarName, &names,
+				func(root string) { roots = append(roots, root) })
 
 		case html.SelfClosingTagToken:
-			// TODO
+			tn, hasAttr := z.TagName()
+			tagName := string(tn)
+			varName := namer.next(tagName)
+
+			err := g.handleStartToken(&funcBuf, z, path, tagName, varName, hasAttr, refs, history)
+			if err != nil {
+				return err
+			}
+
+			g.handleEndToken(&funcBuf, tagName, varName, &names,
+				func(root string) { roots = append(roots, root) })
 
 		case html.CommentToken:
 			// ignore
@@ -329,29 +257,11 @@ tokenizeView:
 		}
 	}
 
-	fmt.Fprintf(&funcBuf, "\n\nreturn &%s{\n", typeName)
-	for k, r := range refs {
-		fmt.Fprintf(&funcBuf, "%s: %s,\n", k, r.VarName)
-	}
-	fmt.Fprintf(&funcBuf, "roots: []*dom.Element{%s},\n", strings.Join(roots, ", "))
-	fmt.Fprint(&funcBuf, "}")
+	writeReturn(&funcBuf, typeName, refs, roots)
 	fmt.Fprint(&funcBuf, "}\n\n")
 
-	var typeBuf bytes.Buffer
-	fmt.Fprintf(&typeBuf, "type %s struct {", typeName)
-	for k, v := range refs {
-		typeName := "*dom.Element" // TODO: make this more specific (like *html.HTMLDomElement)
-		if v.TypeName != "" {
-			typeName = "*" + v.TypeName
-		}
-		fmt.Fprintf(&typeBuf, "%s %s\n", k, typeName)
-	}
-	fmt.Fprint(&typeBuf, "\n")
-	fmt.Fprint(&typeBuf, "roots []*dom.Element\n")
-	fmt.Fprint(&typeBuf, "}\n\n")
-
-	// Add view output to the overall output.
-	io.Copy(&g.viewsBuf, &typeBuf)
+	// Add this view's output to the overall output.
+	writeTypeDefinition(&g.viewsBuf, typeName, refs)
 	io.Copy(&g.viewsBuf, &funcBuf)
 
 	if insideStyle {
@@ -364,6 +274,146 @@ tokenizeView:
 	}
 
 	return nil
+}
+
+func (g *generator) handleStartToken(w io.Writer, z *html.Tokenizer,
+	path, tagName, varName string, hasAttr bool,
+	refs map[string]TagAndVarAndTypeName, history *orderedSet) error {
+
+	if tagName == "include" {
+		return g.handleStartInclude(w, z, path, tagName, varName, hasAttr, refs, history)
+	}
+	return g.handleStartRegular(w, z, tagName, varName, hasAttr, refs)
+}
+
+func (g *generator) handleStartRegular(w io.Writer, z *html.Tokenizer,
+	tagName, varName string, hasAttr bool,
+	refs map[string]TagAndVarAndTypeName) error {
+
+	fmt.Fprintf(w, "%s := _document.CreateElement(%q, nil)\n", varName, tagName)
+	err := attrsFunc(z, hasAttr, func(k, v []byte) error {
+		if equalsRef(k) {
+			v := string(v)
+			if _, ok := disallowedRefNames[v]; ok {
+				return fmt.Errorf("ref name %q disallowed (reserved for internal use)", v)
+			}
+			ex, ok := refs[v]
+			if ok {
+				return fmt.Errorf("ref name %q appears multiple times in component (previous occurence in <%s>)", v, ex.TagName)
+			}
+			refs[v] = TagAndVarAndTypeName{tagName, varName, ""}
+			return nil
+		}
+		fmt.Fprintf(w, "%s.SetAttribute(%q, %q)\n", varName, k, v)
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *generator) handleStartInclude(w io.Writer, z *html.Tokenizer,
+	path, tagName, varName string, hasAttr bool,
+	refs map[string]TagAndVarAndTypeName, history *orderedSet) error {
+
+	var foundPathAttr bool
+	var refAttrVal string
+	var includeTypeName string
+
+	err := attrsFunc(z, hasAttr, func(k, v []byte) error {
+		isRef := equalsRef(k)
+		isPath := equalsPath(k)
+
+		// validate attributes
+		if !isRef && !isPath {
+			return fmt.Errorf("<include> specifies invalid attribute %q", k)
+		}
+
+		if isRef {
+			if _, ok := disallowedRefNames[string(v)]; ok {
+				return fmt.Errorf("ref name %q disallowed (reserved for internal use)", v)
+			}
+			refAttrVal = string(v)
+		} else {
+			foundPathAttr = true
+
+			v := string(v)
+			var includePath string
+			if filepath.IsAbs(v) {
+				includePath = filepath.Join(g.opts.Root, v)
+			} else {
+				includePath = filepath.Join(filepath.Dir(path), v)
+			}
+			err := g.generateOneFile(includePath, history)
+			if err != nil {
+				return err
+			}
+			// ... successfully included; append it
+			includeTypeName = componentTypeName(filepath.Base(includePath))
+			includeConstructorFuncName := constructorFuncName(includeTypeName)
+			fmt.Fprintf(w, "%s := %s()\n", varName, includeConstructorFuncName)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !foundPathAttr {
+		return fmt.Errorf("missing required \"path\" attribute in <include>")
+	}
+	if refAttrVal != "" {
+		ex, ok := refs[refAttrVal]
+		if ok {
+			return fmt.Errorf("ref name %q appears multiple times in component (previous occurence in <%s>)", refAttrVal, ex.TagName)
+		}
+		refs[refAttrVal] = TagAndVarAndTypeName{tagName, varName, includeTypeName}
+	}
+	return nil
+}
+
+func (*generator) handleEndToken(w io.Writer, tagName, varName string, names *stack, addRoot func(string)) {
+	parent, ok := names.peek()
+	if !ok {
+		// no parent; record as root
+		addRoot(varName)
+		return
+	}
+
+	if tagName == "include" {
+		fmt.Fprintf(w, "for _, r := range %s.roots {\n", varName)
+		fmt.Fprintf(w, "%s.AppendChild(&r.Node)\n", parent.VarName)
+		fmt.Fprintf(w, "}\n")
+	} else {
+		fmt.Fprintf(w, "%s.AppendChild(&%s.Node)\n", parent.VarName, varName)
+	}
+}
+
+func writeReturn(w io.Writer, typeName string, refs map[string]TagAndVarAndTypeName, roots []string) {
+	fmt.Fprintf(w, "\n\nreturn &%s{\n", typeName)
+	for k, r := range refs {
+		fmt.Fprintf(w, "%s: %s,\n", k, r.VarName)
+	}
+	fmt.Fprintf(w, "roots: []*dom.Element{%s},\n", strings.Join(roots, ", "))
+	fmt.Fprint(w, "}")
+}
+
+func writeTypeDefinition(w io.Writer, typeName string, refs map[string]TagAndVarAndTypeName) {
+	fmt.Fprintf(w, "type %s struct {", typeName)
+	for k, v := range refs {
+		typeName := "*dom.Element" // TODO: make this more specific (like *html.HTMLDomElement)
+		if v.TypeName != "" {
+			typeName = "*" + v.TypeName
+		}
+		fmt.Fprintf(w, "%s %s\n", k, typeName)
+	}
+	fmt.Fprint(w, "\n")
+	fmt.Fprint(w, "roots []*dom.Element\n")
+	fmt.Fprint(w, "}\n\n")
 }
 
 // varNames returns successive variable names to use in a component.
